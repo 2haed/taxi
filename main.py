14c0740717033b31900b2153378c799e63f5f578
Даниил <101021036+2haed@users.noreply.github.com>
2022-12-23 00:57:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from databases import Database
+import sqlalchemy
+
+from app.db.config import settings
+from app.db.tables import Cars
 
 app = FastAPI()
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql:psycopg2://{settings.database.user}:{settings.database.password}@{settings.database.host}:{settings.database.port}/{settings.database.database}"
+)
+database = Database(SQLALCHEMY_DATABASE_URL)
 
 
-@app.post("/")
-async def root():
-    return {"message": "Hello World"}
+@app.on_event("startup")
+async def startup():
+    await database.connect()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
